560a3ea66b9d1c1b7d6fceb2316e9520b1e88b09
Klyntech <klyntyn09@gmail.com>
2026-06-02 15:18:57 +0100

    Update file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
+from app.routers import books
+
+app = FastAPI(title="Book Library API")
+app.include_router(books.router, prefix="/books", tags=["books"])
